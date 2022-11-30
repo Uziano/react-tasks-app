e8172b6834015e4f77f0dc028d2fb5b7675d0572
Uziano <lucianoperaltadefazio@gmail.com>
2022-11-30 13:41:35 -0300

    First Commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+    {
+        id: 0,
+        title: 'Mi primer task',
+        description: 'primer tarea'
+    },
+    {
+        id: 1,
+        title: 'Mi segundo task',
+        description: 'segunda tarea'
+    },
+    {
+        id: 2,
+        title: 'Mi tercer task',
+        description: 'tercer tarea'
+    },
+]
